5923e7d2fc4cfc4f1bb93e54f9f6f33fe5fbd80c
Giovani Meza <giovanidav5@outlook.com>
2021-06-01 17:53:10 -0600

    added itext jars

--- webClientProyecto/src/java/ModeloCliente/MedicamentoCliente.java
+++ webClientProyecto/src/java/ModeloCliente/MedicamentoCliente.java
@@ -1,15 +1,20 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (C) 2021 
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
-package ModeloCliente;
-
-import javax.xml.ws.WebServiceRef;
-import java.util.List;
-import modeloservicio.Medicamento;
-import modeloservicio.Servicios_Service;
-
 
 /**
  *
@@ -19,6 +24,15 @@ import modeloservicio.Servicios_Service;
  * @author Byron Rosbin Antonio Orozco 9941-19-23685
  * @author Karina Aracely Sanchez Ixcot 9941-19-264
  */
+
+package ModeloCliente;
+
+import javax.xml.ws.WebServiceRef;
+import java.util.List;
+import modeloservicio.Medicamento;
+import modeloservicio.Servicios_Service;
+
+
 
 public class MedicamentoCliente {
 
